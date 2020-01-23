81b1a6d34474a83c1545e2197ed1c8011793edf7
eliran menachem <eliran.menachem@gmail.com>
2020-01-23 16:51:01 +0200

    Video Section With Autoplay Modal

--- js.js
+++ js.js
@@ -2,27 +2,40 @@
 $("#year").text(new Date().getFullYear());
 
 // Init scrollspy
-$('body').scrollspy({target:'#main-nav'});
+$("body").scrollspy({ target: "#main-nav" });
 
 // Smooth scrollong
 $("#main-nav a").on("click", function(event) {
-    if (this.hash !== "") {
-      event.preventDefault();
-      const hash = this.hash;  
-      $("html, body").animate(
-        {
-          scrollTop: $(hash).offset().top
-        },
-        800,
-        function() {
-          window.location.hash = hash;
-        }
-      );
-    }
-  });
+  if (this.hash !== "") {
+    event.preventDefault();
+    const hash = this.hash;
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top
+      },
+      800,
+      function() {
+        window.location.hash = hash;
+      }
+    );
+  }
+});
 
 // Configure Slider
-$('.carousel').carousel({
+$(".carousel").carousel({
   interval: 6 * 1000,
-  pause:'hover'
-})
+  pause: "hover"
+});
+
+//Video play
+$(function() {
+  $(".video").click(function () {
+    var theModal = $(this).data("target"),
+    videoSRC = $(this).attr("data-video"),
+    videoSRCauto = videoSRC + "?modestbranding=1&rel=0&controls=0&showinfo=0&html5=1&autoplay=1";
+    $(theModal + ' iframe').attr('src', videoSRCauto);
+    $(theModal + ' button.close').click(function () {
+      $(theModal + ' iframe').attr('src', videoSRC);
+    });
+  });
+});
